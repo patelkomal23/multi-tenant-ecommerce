963807b009700d2e44544097a7dd956f76efa0f9
patelkomal2332@gmail.com <patelkomal2332@gmail.com>
2026-07-13 19:36:00 +0530

    sertup react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+function App(){
+  return(
+    <div className="bg-blue-500 text- white text-4xl font-bold h-screen flex item-center justify-center"> Tailwind css working</div>
+  );
+}
+export default App;
